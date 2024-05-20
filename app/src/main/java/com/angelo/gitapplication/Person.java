e9cf9c5a8eec48f20f60245655f21ed29215c657
Angelo.Luo <angelo.luo@ttigroup.com>
2024-05-20 15:53:28 +0800

    [20240520]-第二次提交

---
+++ app/src/main/java/com/angelo/gitapplication/Person.java
@@ -0,0 +1,12 @@
+package com.angelo.gitapplication;
+
+/**
+ * author: Angelo.Luo
+ * date : 05/20/2024 3:52 PM
+ * description:
+ */
+public class Person {
+    public static void main(String[] args) {
+
+    }
+}
